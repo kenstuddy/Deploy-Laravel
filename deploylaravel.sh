1a32fca043f1b68f155fe82f00b6cb58ca6cdf20
Ken Studdy <kenstuddy@users.noreply.github.com>
2017-07-07 05:42:50 -0400

    Add files via upload

---
+++ deploylaravel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if ["$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE"]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+sudo mv composer.phar /usr/local/bin/composer
+echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' >> ~/.bashrc
+sudo chown -R ken /home/ken
+sudo chown -R ken /var/www/html
+composer global require "laravel/installer"
+sudo chmod 744 -R /var/www/html
+sudo chown -R www-data:www-data /var/www/html
