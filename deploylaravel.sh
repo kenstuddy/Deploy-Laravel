6fa5486d0303e6cc90949b47d42e2a43f2709c62
Ken Studdy <kenstuddy@users.noreply.github.com>
2017-12-17 19:09:27 -0500

    Added warning to script, removed chown.

--- deploylaravel.sh
+++ deploylaravel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Warning: Do not run this script as root. See: https://getcomposer.org/doc/faqs/how-to-install-untrusted-packages-safely.md
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
@@ -15,8 +16,6 @@ RESULT=$?
 rm composer-setup.php
 sudo mv composer.phar /usr/local/bin/composer
 echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' >> ~/.bashrc
-sudo chown -R ken /home/ken
-sudo chown -R ken /var/www/html
 composer global require "laravel/installer"
 find /var/www/html \( -type f -execdir chmod 644 {} \; \) \
                   -o \( -type d -execdir chmod 711 {} \; \)
